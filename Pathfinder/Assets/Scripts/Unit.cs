d64ed7980cd33c9361ab6aa286663d97458e66ad
Golen <golen87@hotmail.com>
2018-11-07 15:18:20 +0100

    Added Unit looking direction

--- Pathfinder/Assets/Scripts/Unit.cs
+++ Pathfinder/Assets/Scripts/Unit.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour {
-	private float speed = 1.0f;
+	private float speed = 2.0f;
 	private Vector3 goalPosition;
+	private Vector3 direction;
 
 
 	// Use this for initialization
 	void Start () {
 		this.goalPosition = this.transform.position;
+		this.direction.Set(1, 0, 0);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Random.Range(0.0f, 1.0f) < 0.02) {
-			this.goalPosition += new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		if (this.goalPosition != this.transform.position) {
+
+			Vector3 newPos = Vector3.MoveTowards( this.transform.position, this.goalPosition, this.speed * Time.deltaTime );
+			this.transform.position = newPos;
+		}
+		else if (Random.Range(0.0f, 1.0f) < 0.02) {
+			this.goalPosition += new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+			this.direction = this.goalPosition - this.transform.position;
 		}
 
-		Vector3 newPos = Vector3.MoveTowards( this.transform.position, this.goalPosition, this.speed * Time.deltaTime );
-		this.transform.position = newPos;
+		this.transform.forward = Vector3.Lerp(
+			this.transform.forward,
+			this.direction,
+			10 * Time.deltaTime
+		);
 	}
 
 	public void SetGoalPosition(Vector3 goal) {
 		this.goalPosition = goal;
+		this.direction = this.goalPosition - this.transform.position;
 	}
 }

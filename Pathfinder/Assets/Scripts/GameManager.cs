7c4495a02e93af38ad5f2ab63399246217af09ac
David Pop <david.pop@icloud.com>
2018-11-07 14:02:35 +0100

    Added initial ground plane generation

--- Pathfinder/Assets/Scripts/GameManager.cs
+++ Pathfinder/Assets/Scripts/GameManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public float groundPlaneSize = 100.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        createGroundPlane();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void createGroundPlane(){
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane.name = "GroundPlane";
+
+        float scaleFactor = groundPlaneSize / 10;
+        float translation = groundPlaneSize / 2;
+
+        plane.transform.localScale = new Vector3(scaleFactor, 1, scaleFactor);
+        plane.transform.position = new Vector3(translation, 0, translation);
+    }
 }

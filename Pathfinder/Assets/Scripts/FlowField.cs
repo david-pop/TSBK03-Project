d5085052960668b75ae990ecbbd925dad434f237
Golen <golen87@hotmail.com>
2018-11-08 16:07:48 +0100

    Made flowfield breadth-first

--- Pathfinder/Assets/Scripts/FlowField.cs
+++ Pathfinder/Assets/Scripts/FlowField.cs
@@ -2,81 +2,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct Node {
+	public int x;
+	public int z;
+	public float cost;
+
+	public Node(int x, int z, float cost) {
+		this.x = x;
+		this.z = z;
+		this.cost = cost;
+	}
+}
+
 public class FlowField {
+	public const float SQRT_2 = 1.41421356f;
 
-    private int[,] integratorField;
-    private int width, height;
-    private int[,] obstacleField;
-    private int cellSize;
+	private float[,] integratorField;
+	private int width, height;
+	private int[,] obstacleField;
+	private int cellSize;
+	private Queue<Node> searchQueue;
 
-    public FlowField(int[,] obstacleField, int cellSize, int goalX, int goalZ){
-        width = obstacleField.GetLength(0);
-        height = obstacleField.GetLength(1);
 
-        integratorField = new int[width, height];
+	public FlowField(int[,] obstacleField, int cellSize, int goalX, int goalZ) {
+		width = obstacleField.GetLength(0);
+		height = obstacleField.GetLength(1);
+		this.cellSize = cellSize;
 
-        for (int x = 0; x < width; x++){
-            for (int z = 0; z < height; z++){
-                integratorField[x, z] = int.MaxValue;
-            }
-        }
+		integratorField = new float[width, height];
 
-        this.obstacleField = obstacleField;
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < height; z++) {
+				integratorField[x, z] = float.MaxValue;
+			}
+		}
 
-        integrate(goalX, goalZ, 0);
-    }
+		this.obstacleField = obstacleField;
 
-    private void integrate(int x, int z, int cost){
-        if(x >  width - 1 || x < 0 ||
-           z > height - 1 || z < 0){
-            return;
-        }
+		searchQueue = new Queue<Node>();
+		searchQueue.Enqueue( new Node(goalX, goalZ, 0.0f) );
 
-        if(obstacleField[x, z] == 1){
-            return;
-        }
+		while (searchQueue.Count > 0) {
+			Node node = searchQueue.Dequeue();
+			integrate(node.x, node.z, node.cost);
+		}
+	}
 
-        if(cost > integratorField[x,z]){
-            integratorField[x, z] = cost;
+	private void integrate(int x, int z, float cost) {
+		//Debug.Log("(" + x + ", " + z + ")" + " " + cost);
 
-            integrate(x + 1, z, cost + 1);
-            integrate(x - 1, z, cost + 1);
-            integrate(x, z + 1, cost + 1);
-            integrate(x, z - 1, cost + 1);
+		if (!isInside(x, z)) {
+			return;
+		}
 
-            integrate(x + 1, z + 1, cost + 1);
-            integrate(x - 1, z + 1, cost + 1);
-            integrate(x - 1, z - 1, cost + 1);
-            integrate(x + 1, z - 1, cost + 1);
-        }
-        else{
-            return;
-        }
-    }
+		if (integratorField[x, z] != float.MaxValue) {
+			return;
+		}
 
-    public Vector2 getDirection(int x, int z){
-        if (x > width - 1 || x < 0 ||
-           z > height - 1 || z < 0){
-            return Vector2.zero;
-        }else{
-            int left = ((x - 1) < 0 || obstacleField[x - 1, z] == 1) ? 
-                integratorField[x, z] : integratorField[x - 1, z];
+		if (obstacleField[x, z] != 0) {
+			return;
+		}
 
-            int right = ((x + 1) > width || obstacleField[x + 1, z] == 1) ?
-                integratorField[x, z] : integratorField[x + 1, z];
+		integratorField[x,z] = cost;
 
-            int bottom = ((z - 1) < 0 || obstacleField[x, z - 1] == 1) ?
-                integratorField[x, z] : integratorField[x, z - 1];
+		searchQueue.Enqueue( new Node(x + 1, z, cost + 1) );
+		searchQueue.Enqueue( new Node(x - 1, z, cost + 1) );
+		searchQueue.Enqueue( new Node(x, z + 1, cost + 1) );
+		searchQueue.Enqueue( new Node(x, z - 1, cost + 1) );
+		searchQueue.Enqueue( new Node(x + 1, z + 1, cost + SQRT_2) );
+		searchQueue.Enqueue( new Node(x - 1, z + 1, cost + SQRT_2) );
+		searchQueue.Enqueue( new Node(x - 1, z - 1, cost + SQRT_2) );
+		searchQueue.Enqueue( new Node(x + 1, z - 1, cost + SQRT_2) );
+	}
 
-            int top = ((z + 1) > height || obstacleField[x, z + 1] == 1) ?
-                integratorField[x, z] : integratorField[x, z + 1];
+	public bool isAccessible(int x, int z) {
+		return this.integratorField[x, z] != float.MaxValue;
+	}
 
-            return new Vector2(left - right, bottom - top);
-        }
-    }
+	public float getCost(int x, int z) {
+		return this.integratorField[x, z];
+	}
 
-    public Vector2 getDirection(float x, float z){
-        return getDirection(
-            Mathf.FloorToInt(x / cellSize), Mathf.FloorToInt(z / cellSize));
-    }
+	public Vector3 getDirection(int x, int z) {
+		if (isInside(x, z) && isAccessible(x, z))
+		{
+			float left = ((x - 1) < 0 || obstacleField[x - 1, z] != 0) ? 
+				integratorField[x, z] : integratorField[x - 1, z];
+
+			float right = ((x + 1) >= width || obstacleField[x + 1, z] != 0) ?
+				integratorField[x, z] : integratorField[x + 1, z];
+
+			float bottom = ((z - 1) < 0 || obstacleField[x, z - 1] != 0) ?
+				integratorField[x, z] : integratorField[x, z - 1];
+
+			float top = ((z + 1) >= height || obstacleField[x, z + 1] != 0) ?
+				integratorField[x, z] : integratorField[x, z + 1];
+
+			return new Vector3(left - right, 0, bottom - top);
+		}
+
+		return Vector3.zero;
+	}
+
+	public Vector3 getDirection(float x, float y, float z) {
+		return getDirection(
+			Mathf.FloorToInt(x / cellSize), Mathf.FloorToInt(z / cellSize)
+		);
+	}
+
+	private bool isInside(int x, int z) {
+		return (x >= 0 && x < width && z >= 0 && z < height);
+	}
 }

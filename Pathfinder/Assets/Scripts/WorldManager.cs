8dafdfefaeb3aae3d1900445d36dd66186e7e90e
Golen <golen87@hotmail.com>
2018-11-08 16:10:56 +0100

    Added flowfield example generation

--- Pathfinder/Assets/Scripts/WorldManager.cs
+++ Pathfinder/Assets/Scripts/WorldManager.cs
@@ -12,15 +12,19 @@ public class WorldManager : MonoBehaviour {
 
 	private int[,] worldGrid;
 
+	public GameObject arrow;
+
 	// Use this for initialization
 	void Start () {
-		if(Instance == null){
+		if (Instance == null) {
 			Instance = this;
-		}else if(Instance != this){
+		} else if (Instance != this) {
 			Destroy(gameObject);
 		}
 
-		int seed = Random.Range(0, 10000);
+		createGroundPlane();
+
+		float seed = Random.Range(0.0f, 10000.0f);
 
 		worldGrid = new int[GridSize, GridSize];
 		int borderSize = 10;
@@ -33,7 +37,7 @@ public class WorldManager : MonoBehaviour {
 				p += Mathf.Max(
 					Mathf.Max(0, Mathf.Abs(x - GridSize/2) - GridSize/2 + 4),
 					Mathf.Max(0, Mathf.Abs(z - GridSize/2) - GridSize/2 + 4)
-				) / 8.0f;
+				) / 6.0f;
 
 				p = Mathf.Atan((p-0.5f)*2.0f)/2.0f+0.5f;
 
@@ -47,7 +51,7 @@ public class WorldManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		FlowField ff = new FlowField( this.worldGrid, 1, this.GridSize/2, this.GridSize/2 );
 	}
 
 	private void addSquareObstacleOfSizeAtPosition(int size, int gridX, int gridZ, float height){
@@ -65,6 +69,46 @@ public class WorldManager : MonoBehaviour {
 				);
 				newCube.transform.localScale = new Vector3(1.0f, 1+(int)(height*8), 1.0f);
 				newCube.GetComponent<MeshRenderer>().material = obstacleMaterial;	
+			}
+		}
+	}
+
+	private void createGroundPlane() {
+		GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		plane.name = "GroundPlane";
+
+		float scaleFactor = GridSize / 10;
+		float translation = GridSize / 2;
+
+		plane.transform.localScale = new Vector3(scaleFactor, 1, scaleFactor);
+		plane.transform.position = new Vector3(translation, 0, translation);
+	}
+
+	private void debugFlowfield(FlowField ff) {
+		// Debug map
+		for (int x = 0; x < this.GridSize; x++) {
+			for (int z = 0; z < this.GridSize; z++) {
+				if (ff.isAccessible(x, z)) {
+					Vector3 dir = ff.getDirection(x, z);
+
+					GameObject newArrow = Instantiate(arrow);
+					newArrow.transform.position = new Vector3( x + 0.5f, ff.getCost(x,z)/10.0f, z + 0.5f );
+
+					newArrow.transform.forward = dir;
+					Vector3 s = newArrow.transform.localScale;
+					s.z = 0.1f + dir.magnitude/6.0f;
+					newArrow.transform.localScale = s;
+
+					//newArrow.GetComponent<Renderer>().material.color = Color.black;
+					GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+					obj.transform.position = new Vector3(
+						x + 0.5f,
+						0,
+						z + 0.5f
+					);
+					obj.transform.localScale = new Vector3(0.5f, ff.getCost(x,z)/10.0f, 0.5f);
+					obj.GetComponent<Renderer>().material.color = Color.HSVToRGB((ff.getCost(x,z)/50.0f)%1, 1, 1);
+				}
 			}
 		}
 	}

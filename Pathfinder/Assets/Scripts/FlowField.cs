dd8fda5496f9260b270fbe5c553bc8fe0e2f564f
Golen <golen87@hotmail.com>
2018-11-09 14:54:29 +0100

    Added bilinear interpolarization for flowfield cost and direction.

--- Pathfinder/Assets/Scripts/FlowField.cs
+++ Pathfinder/Assets/Scripts/FlowField.cs
@@ -18,19 +18,19 @@ public struct Node {
 public class FlowField {
 	public const float SQRT_2 = 1.41421356f;
 
-	private float[,] integratorField;
 	private int width, height;
+	private float[,] integratorField;
 	private int[,] obstacleField;
+	private bool[,] visited;
 	private int cellSize;
 	private Queue<Node> searchQueue;
 
 
-    public FlowField(int[,] obstacleField, int cellSize, float goalX, float goalZ):
-    this(obstacleField, cellSize, 
-         Mathf.FloorToInt(goalX / cellSize), 
-         Mathf.FloorToInt(goalZ / cellSize)){
-
-    }
+	public FlowField(int[,] obstacleField, int cellSize, float goalX, float goalZ):
+	this(obstacleField, cellSize,
+		 Mathf.FloorToInt(goalX / cellSize),
+		 Mathf.FloorToInt(goalZ / cellSize)){
+	}
 
 	public FlowField(int[,] obstacleField, int cellSize, int goalX, int goalZ) {
 		width = obstacleField.GetLength(0);
@@ -38,10 +38,12 @@ public class FlowField {
 		this.cellSize = cellSize;
 
 		integratorField = new float[width, height];
+		visited = new bool[width, height];
 
 		for (int x = 0; x < width; x++) {
 			for (int z = 0; z < height; z++) {
 				integratorField[x, z] = float.MaxValue;
+				visited[x, z] = false;
 			}
 		}
 
@@ -54,6 +56,26 @@ public class FlowField {
 			Node node = searchQueue.Dequeue();
 			integrate(node.x, node.z, node.cost);
 		}
+
+		// Increases cost near walls. Doesn't work very well.
+		/*
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < height; z++) {
+				if (isAccessible(x, z)) {
+					int wallCount = 0;
+					for (int dx = x-1; dx <= x+1; dx++) {
+						for (int dz = z-1; dz <= z+1; dz++) {
+							if (!isAccessible(dx, dz)) {
+								if (wallCount < 1)
+									this.integratorField[x, z] += 1.0f;
+								wallCount += 1;
+							}
+						}
+					}
+				}
+			}
+		}
+		*/
 	}
 
 	private void integrate(int x, int z, float cost) {
@@ -71,6 +93,7 @@ public class FlowField {
 			return;
 		}
 
+		visited[x, z] = true;
 		integratorField[x,z] = cost;
 
 		searchQueue.Enqueue( new Node(x + 1, z, cost + 1) );
@@ -84,27 +107,43 @@ public class FlowField {
 	}
 
 	public bool isAccessible(int x, int z) {
-		return this.integratorField[x, z] != float.MaxValue;
+		return isInside(x, z) && this.visited[x, z];
 	}
 
-	public float getCost(int x, int z) {
-		return this.integratorField[x, z];
+	public bool isAccessible(float x, float z) {
+		return isAccessible((int)x, (int)z);
 	}
 
-	public Vector3 getDirection(int x, int z) {
-		if (isInside(x, z) && isAccessible(x, z))
+	public float getCost(float fx, float fz) {
+		fx -= 0.5f;
+		fz -= 0.5f;
+		int ix = Mathf.FloorToInt(fx);
+		int iz = Mathf.FloorToInt(fz);
+
+		float lowestCost = float.MaxValue;
+		for (int dx = ix; dx <= ix+1; dx++)
+			for (int dz = iz; dz <= iz+1; dz++)
+				if (isAccessible(dx, dz))
+					lowestCost = Mathf.Min( lowestCost, this.integratorField[dx, dz] );
+
+		float obstacleCost = lowestCost + 10.0f;
+		float A = isAccessible(ix+0, iz+0) ? this.integratorField[ix+0, iz+0] : obstacleCost;
+		float B = isAccessible(ix+1, iz+0) ? this.integratorField[ix+1, iz+0] : obstacleCost;
+		float C = isAccessible(ix+1, iz+1) ? this.integratorField[ix+1, iz+1] : obstacleCost;
+		float D = isAccessible(ix+0, iz+1) ? this.integratorField[ix+0, iz+1] : obstacleCost;
+
+		float value = Utils.QuadLerp( A, B, C, D, fx - ix, fz - iz );
+		return value;
+	}
+
+	public Vector3 getDirection(float x, float z) {
+		if (isAccessible(x, z))
 		{
-			float left = ((x - 1) < 0 || obstacleField[x - 1, z] != 0) ? 
-				integratorField[x, z] : integratorField[x - 1, z];
-
-			float right = ((x + 1) >= width || obstacleField[x + 1, z] != 0) ?
-				integratorField[x, z] : integratorField[x + 1, z];
-
-			float bottom = ((z - 1) < 0 || obstacleField[x, z - 1] != 0) ?
-				integratorField[x, z] : integratorField[x, z - 1];
-
-			float top = ((z + 1) >= height || obstacleField[x, z + 1] != 0) ?
-				integratorField[x, z] : integratorField[x, z + 1];
+			float d = 0.1f;
+			float left   = getCost( x-d, z );
+			float right  = getCost( x+d, z );
+			float bottom = getCost( x, z-d );
+			float top    = getCost( x, z+d );
 
 			return new Vector3(left - right, 0, bottom - top).normalized;
 		}
@@ -113,9 +152,7 @@ public class FlowField {
 	}
 
 	public Vector3 getDirection(Vector3 pos) {
-		return getDirection(
-			Mathf.FloorToInt(pos.x / cellSize), Mathf.FloorToInt(pos.z / cellSize)
-		);
+		return getDirection(pos.x, pos.z);
 	}
 
 	private bool isInside(int x, int z) {
--- Pathfinder/Assets/Scripts/WorldManager.cs
+++ Pathfinder/Assets/Scripts/WorldManager.cs
@@ -47,8 +47,12 @@ public class WorldManager : MonoBehaviour {
 				}
 			}
 		}
+
+		//Vector3 goal = GetRandomAccessible();
+		//FlowField ff = new FlowField( this.worldGrid, 1, goal.x, goal.z );
+		//DebugFlowfield(ff);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	}
@@ -67,7 +71,7 @@ public class WorldManager : MonoBehaviour {
 					z * CellSize + CellSize / 2.0f
 				);
 				newCube.transform.localScale = new Vector3(1.0f, 1+(int)(height*8), 1.0f);
-				newCube.GetComponent<MeshRenderer>().material = obstacleMaterial;	
+				newCube.GetComponent<MeshRenderer>().material = obstacleMaterial;
 			}
 		}
 	}
@@ -87,30 +91,55 @@ public class WorldManager : MonoBehaviour {
 		return (this.worldGrid[x, z] == 0);
 	}
 
-	public void debugFlowfield(FlowField ff) {
-		// Debug map
-		for (int x = 0; x < this.GridSize; x++) {
-			for (int z = 0; z < this.GridSize; z++) {
-				if (ff.isAccessible(x, z)) {
-					Vector3 dir = ff.getDirection(x, z);
+	public Vector3 GetRandomAccessible() {
+		for (int tries=0; tries<10000; tries++) {
+			int x = Random.Range(0, this.GridSize);
+			int z = Random.Range(0, this.GridSize);
+			if ( IsAccessible(x, z) ) {
+				return new Vector3(x, 0, z);
+			}
+		}
+		return Vector3.zero;
+	}
 
+	public void DebugFlowfield(FlowField ff) {
+		int density = 3;
+
+		for (float x = 0; x <= this.GridSize; x+=1.0f/density) {
+			for (float z = 0; z <= this.GridSize; z+=1.0f/density) {
+				float offset = 0.5f / density;
+				float px = x + offset;
+				float pz = z + offset;
+				Vector3 dir = ff.getDirection( px, pz );
+				float cost = ff.getCost( px, pz );
+
+				if (cost < float.MaxValue) {
+
+					/*
 					GameObject newArrow = Instantiate(arrow);
-					newArrow.transform.position = new Vector3( x + 0.5f, ff.getCost(x,z)/10.0f, z + 0.5f );
+					newArrow.transform.position = new Vector3( px, cost/50.0f, pz );
 
-					newArrow.transform.forward = dir;
+					if (dir.sqrMagnitude > 0) {
+						newArrow.transform.forward = dir;
+					}
 					Vector3 s = newArrow.transform.localScale;
+					if (dir.magnitude == Mathf.Infinity)
+						dir.Set(0,1,0);
 					s.z = 0.1f + dir.magnitude/6.0f;
+					s *= 3.0f / density;
 					newArrow.transform.localScale = s;
-
+					Renderer[] cs = newArrow.GetComponentsInChildren<Renderer>();
+					foreach (Renderer c in cs) {
+						c.material.color = Color.HSVToRGB((cost/100.0f)%1, 1, 1);
+					}
 					//newArrow.GetComponent<Renderer>().material.color = Color.black;
+					*/
+
 					GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-					obj.transform.position = new Vector3(
-						x + 0.5f,
-						0,
-						z + 0.5f
-					);
-					obj.transform.localScale = new Vector3(0.5f, ff.getCost(x,z)/10.0f, 0.5f);
-					obj.GetComponent<Renderer>().material.color = Color.HSVToRGB((ff.getCost(x,z)/50.0f)%1, 1, 1);
+					obj.transform.position = new Vector3( px, 0, pz );
+					obj.transform.localScale = new Vector3( 0.5f/density, cost/10.0f, 0.5f/density );
+					float v = ff.isAccessible( (int)px, (int)pz ) ? 1.0f : 0.3f;
+					obj.GetComponent<Renderer>().material.color = Color.HSVToRGB( (cost/10.0f)%1, 1, v );
 				}
 			}
 		}

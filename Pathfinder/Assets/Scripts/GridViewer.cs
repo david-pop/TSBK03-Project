075774f180a9cfed6444118191fa562a8a850305
Golen <golen87@hotmail.com>
2018-12-06 14:03:37 +0100

    Fixed GridViewer bug.

--- Pathfinder/Assets/Scripts/GridViewer.cs
+++ Pathfinder/Assets/Scripts/GridViewer.cs
@@ -5,26 +5,26 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshFilter))]
 public class GridViewer : MonoBehaviour
 {
-	public int GridSize;
 	public Color GridColor;
 
-	void Awake()
+	void Start()
 	{
+		int GridSize = WorldManager.Instance.GridSize;
 		MeshFilter filter = gameObject.GetComponent<MeshFilter>();
 		var mesh = new Mesh();
 		var verticies = new List<Vector3>();
 
 		var indicies = new List<int>();
-		for (int i = 0; i <= this.GridSize; i++)
+		for (int i = 0; i <= GridSize; i++)
 		{
 			verticies.Add(new Vector3(i, 0, 0));
-			verticies.Add(new Vector3(i, 0, this.GridSize));
+			verticies.Add(new Vector3(i, 0, GridSize));
 
 			indicies.Add(4 * i + 0);
 			indicies.Add(4 * i + 1);
 
 			verticies.Add(new Vector3(0, 0, i));
-			verticies.Add(new Vector3(this.GridSize, 0, i));
+			verticies.Add(new Vector3(GridSize, 0, i));
 
 			indicies.Add(4 * i + 2);
 			indicies.Add(4 * i + 3);
